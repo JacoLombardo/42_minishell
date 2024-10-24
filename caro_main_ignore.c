e545aada85a810d743b8622e18d930d801e28f39
Carolina Silva Rode <csilva-r@c4c6c6.42berlin.de>
2024-10-24 17:29:51 +0200

    better. still 2 mysterious leaks and 4 extra lines

--- caro_main_ignore.c
+++ caro_main_ignore.c
@@ -1,14 +1,15 @@
 #include "minishell.h"
 
-int	main2(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	char	*line;
-	t_full_cmd	*full_cmd;
+	//t_full_cmd	*full_cmd;
 
 	if (!argc && !argv && !env)
 		return (1);
+
+	printf("%s\n", expand_vars(argv[1], env));
 	line = ft_strdup(argv[1]);
-	full_cmd = parse(line, env);
-	print_jacopo(full_cmd, 0);
+	//full_cmd = parse(line, env);
 	return (0);
 }
--- source/expanding/expand.c
+++ source/expanding/expand.c
@@ -19,54 +19,78 @@ int	is_bash_valid(char c)
 	return (FALSE);
 }
 
+char	*get_var_name(int i, char *line)
+{
+	int		var_name_len;
+	char	*temp;
+	char	*var_name;
+
+	var_name_len = 0;
+	while (is_bash_valid(line[i]))
+	{
+		var_name_len++;
+		i++;
+	}
+	temp = ft_strjoin(ft_substr(line, i - var_name_len, var_name_len), "=");
+	var_name = ft_strjoin(temp, "=");
+	free(temp);
+	return (var_name);
+}
+
+char	*append_value(char *new_line, char *var_value, char *var_name)
+{
+	char	*temp;
+
+	temp = ft_strdup(new_line);
+	free(new_line);
+	new_line = ft_strjoin(temp, var_value + ft_strlen(var_name) + 1);
+	free(temp);
+	free(var_name);
+	free(var_value);
+	return (new_line);
+}
+
+char	*append_char(char *new_line, char c)
+{
+	char	*temp;
+
+	temp = ft_strdup(new_line);
+	free(new_line);
+	new_line = ft_charjoin(temp, c);
+	free(temp);
+	return (new_line);
+}
+
 char	*expand_vars(char *line, char *env[])
 {
 	int		j;
 	int		i;
-	int		var_name_len;
 	char	*var_name;
 	char	*var_value;
 	char	*new_line;
-	char	*temp;
 
 	i = 0;
-	var_name_len = 0;
 	new_line = ft_strdup("");
 	while (line[i])
 	{
 		if (line[i] == '$' && what_quotes(line + i + 1) != 2)
 		{
 			i++;
-			while (is_bash_valid(line[i]))
-			{
-				var_name_len++;
-				i++;
-			}
-			temp = ft_substr(line, i - var_name_len, var_name_len);
-			var_name = ft_strjoin(temp, "=");
-			free(temp);
+			var_name = get_var_name(i, line);
 			j = 0;
 			while (env[j])
 			{
 				var_value = ft_strnstr(env[j], var_name, ft_strlen(env[j]));
-				if (!var_value)
-					j++;
-				else
+				if (var_value)
 				{
-					temp = ft_strdup(new_line);
-					free(new_line);
-					new_line = ft_strjoin(temp, var_value + var_name_len + 1);
-					free(temp);
+					new_line = append_value(new_line, var_value, var_name);
 					break ;
 				}
+				j++;
 			}
-			free(var_name);
-			var_name_len = 0;
+			//free(var_name);
 		}
-		temp = ft_strdup(new_line);
-		free(new_line);
-		new_line = ft_charjoin(temp, line[i]);
-		free(temp);
+		new_line = append_char(new_line, line[i]);
 		i++;
 	}
 	return (new_line);
