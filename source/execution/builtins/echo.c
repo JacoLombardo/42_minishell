d2bc126964f0bea6cf380d0efa3febfa2d5b0c3b
Jacopo Lombardo <jalombar@c3a3c10.42berlin.de>
2024-11-06 17:15:06 +0100

    bug fix1

--- source/execution/builtins/echo.c
+++ source/execution/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:28:13 by jalombar          #+#    #+#             */
-/*   Updated: 2024/11/06 15:49:12 by jalombar         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:59:08 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,23 @@
 int	ft_echo(t_full_cmd *cmd, t_data *data)
 {
 	int		i;
-	char	*line;
-	char	*temp;
 
 	(void)data;
-	if (!ft_strcmp(cmd->args[1], "-n"))
+	if (cmd->args[1] && !ft_strcmp(cmd->args[1], "-n"))
 		i = 2;
 	else
 		i = 1;
-	line = ft_strdup(cmd->args[i++]);
 	while (cmd->args[i])
 	{
-		temp = line;
-		line = ft_strjoin(temp, cmd->args[i]);
-		if (!line)
-			return (1);
-		free(temp);
+		printf("%s", cmd->args[i]);
 		i++;
+		if (cmd->args[i])
+			printf(" ");
 	}
-	if (!ft_strcmp(cmd->args[1], "-n"))
-		printf("%s", line);
+	if (cmd->args[1] && !ft_strcmp(cmd->args[1], "-n"))
+		printf("");
 	else
-		printf("%s\n", line);
-	free(line);
+		printf("\n");
 	return (0);
 }
+
